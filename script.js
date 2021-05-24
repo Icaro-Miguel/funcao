e23a6b230c195e4bd7e0244cb6540b1ba783a37f
Icaro-Miguel <80598515+Icaro-Miguel@users.noreply.github.com>
2021-05-24 15:51:14 -0300

    adicionando o arquivo na web.
    adicionando o arquivo com site na web

---
+++ script.js
@@ -0,0 +1,17 @@
+let a = parseInt(prompt('Digite um número inteiro.'));
+let b = parseInt(prompt('digite outro número inteiro.'));
+
+// faz somente a multiplicação.
+
+function calcular(a, b){
+    return a*b;
+
+      
+}
+
+calcular(
+    alert(`o resultado é ${a*b}`)
+);
+
+console.log(a*b);
+
